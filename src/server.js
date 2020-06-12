d7fca0bc6e5fc6a8e7a62275ed2afbe89ab5ec3d
Adriano-Matias <adriano-matias-souza@hotmail.com>
2020-06-12 13:16:06 -0300

    Apliquei template engine nas pages

--- src/server.js
+++ src/server.js
@@ -4,20 +4,27 @@ const server = express();
 // configurar pasta public
 server.use(express.static("public"))
 
+//Utilizando template engine
+const nunjucks = require('nunjucks')
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //configurar caminhos da minha aplicação
 //pagina inicial
 //req: Requisição
 //res: Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", { title: "Busque por um ponto de doação"})
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
-server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 //ligar o server
